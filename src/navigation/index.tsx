c47b2cdc743eddb1b3ac98205054f789e9f4f603
sushilpadaskar <sushilpadaskar07@gmail.com>
2025-09-05 14:32:47 +0530

    Add navigation setup with tabs and stack

---
+++ src/navigation/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import ProductsScreen from "../screens/ProductsScreen";
+import CartScreen from "../screens/CartScreen";
+import UsersScreen from "../screens/UsersScreen";
+import UserDetailsScreen from "../screens/UserDetailsScreen";
+import TokenScreen from "../screens/TokenScreen";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function TabsNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Products" component={ProductsScreen} />
+      <Tab.Screen name="Cart" component={CartScreen} />
+      <Tab.Screen name="Users" component={UsersScreen} />
+      <Tab.Screen name="Token" component={TokenScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function RootNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Root"
+          component={TabsNavigator}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="UserDetails"
+          component={UserDetailsScreen}
+          options={{ title: "User Details" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
